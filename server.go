c99f12895cf239a21b3f99833c8731c8e12bc78a
yangchengkai1 <1351599172@qq.com>
2020-02-09 17:30:20 +0800

    the front page

---
+++ server.go
@@ -0,0 +1,78 @@
+package main
+
+import (
+	"log"
+	"net/http"
+	"time"
+
+	"github.com/gorilla/websocket"
+	"github.com/yangchengkai1/go-websocket/impl"
+)
+
+var (
+	upgrader = websocket.Upgrader{
+		// 读取存储空间大小
+		ReadBufferSize: 1024,
+		// 写入存储空间大小
+		WriteBufferSize: 1024,
+		// 允许跨域
+		CheckOrigin: func(r *http.Request) bool {
+			return true
+		},
+	}
+)
+
+func wsHandler(w http.ResponseWriter, r *http.Request) {
+	var (
+		wsConn *websocket.Conn
+		err    error
+		// data []byte
+		conn *impl.Connection
+		data []byte
+	)
+	// 完成http应答，在httpheader中放下如下参数
+	if wsConn, err = upgrader.Upgrade(w, r, nil); err != nil {
+		return // 获取连接失败直接返回
+	}
+
+	if conn, err = impl.InitConnection(wsConn); err != nil {
+		goto ERR
+	}
+
+	go func() {
+		var (
+			err error
+		)
+		for {
+			// 每隔一秒发送一次心跳
+			if err = conn.WriteMessage([]byte("heartbeat")); err != nil {
+				return
+			}
+			time.Sleep(1 * time.Second)
+		}
+
+	}()
+
+	for {
+		if data, err = conn.ReadMessage(); err != nil {
+			goto ERR
+		}
+		if err = conn.WriteMessage(data); err != nil {
+			goto ERR
+		}
+	}
+
+ERR:
+	// 关闭当前连接
+
+}
+
+func main() {
+	// 当有请求访问ws时，执行此回调方法
+	http.HandleFunc("/ws", wsHandler)
+	// 监听127.0.0.1:7777
+	err := http.ListenAndServe("0.0.0.0:7777", nil)
+	if err != nil {
+		log.Fatal("ListenAndServe", err.Error())
+	}
+}
